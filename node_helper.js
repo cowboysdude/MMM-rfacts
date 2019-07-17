623f17925785f3d115a9ef74b6d5f30b9cf5ceb2
Dirk <dirk.kovert@gmail.com>
2019-07-17 23:14:24 +0200

    switched to @vitalets google-translate-api

--- node_helper.js
+++ node_helper.js
@@ -2,12 +2,12 @@
  * Module: MMM-rfacts
  *
  * By Cowboysdude
- * 
+ *
  */
 const NodeHelper = require('node_helper');
 const request = require('request');
 const parser = require('xml2js').parseString;
-const translate = require('google-translate-api'); 
+const translate = require('@vitalets/google-translate-api');
 
 module.exports = NodeHelper.create({
 
@@ -15,7 +15,7 @@ module.exports = NodeHelper.create({
         console.log("Starting module: " + this.name);
     },
 
-    
+
     getFact: function(url) {
         var self = this;
         request({
@@ -28,16 +28,16 @@ module.exports = NodeHelper.create({
                     if (result.hasOwnProperty('facts')) {
                         var result = JSON.parse(JSON.stringify(result.facts.fact));
                         for (var i = 0; i < 1; i++) {
-                            var result = result[i]; 
+                            var result = result[i];
                             if (config.language != 'en') {
                                 Promise.all([
                                     translate(result, { from: 'en', to: config.language })
                                 ]).then(function(result) {
-                                    var results = JSON.stringify(result[0].text);  
+                                    var results = JSON.stringify(result[0].text);
                                     self.sendSocketNotification("FACT_RESULT", results);
                                 }).catch(function(e) {
                                     console.log("[MMM-rfacts] Translation ERROR! Translation failed, will fall back to English.");
-				    self.sendSocketNotification("FACT_RESULT", result);
+				                            self.sendSocketNotification("FACT_RESULT", result);
                                 })
                             } else {
                                 self.sendSocketNotification("FACT_RESULT", result);
